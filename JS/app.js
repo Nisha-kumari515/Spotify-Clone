40e4859024c967058f3d3f14950ce926685e621e
Nisha-kumari515 <nishakumari10777@gmail.com>
2026-02-06 19:19:19 +0530

    this is spotify clone

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+// let heroes = [["ironman", "spideerman", "thor"],
+//  ["superman" , "wonder woman" , "flash"]];
+
+//  for(let i=0; i<heroes.length ; i++){
+//     console.log(i , heroes[i], heroes[i].length);
+//     for(let j=0 ; j < heroes[i].length ; j++){
+//         console.log(`j = ${j} ${heroes[i][j]}`);
+//     }
+//  }
+
+// let fruits = ["mango" ,"apple" , "banana" , "litchi" , "orange"];
+
+// for(fruit of fruits){
+//     console.log(fruit);
+// }
+
+
+let heroes = [["ironman", "spideerman", "thor"],
+ ["superman" , "wonder woman" , "flash"]];
+  for(list of heroes){
+    for(name of list){
+        console.log(name);
+    }
+  }
---
+++ TODO APP/script.js
@@ -0,0 +1,24 @@
+let todo = [];
+
+let req = prompt("Please enter your request");
+
+while(true) {
+    if(req == "quit"){
+        console.log("quitting app");
+        break;
+    }
+
+    if(req == "List"){
+        console.log("-------------");
+        for(task of todo){
+            console.log(task);
+        }
+        console.log("-------------");
+    } else if(req == "add"){
+        let task = prompt("please enter the task you want to add");
+        todo.push(task);
+        console.log("task added");
+    } else if(req == "delete"){
+
+    }
+}
